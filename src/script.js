b12d99377cff295c4afa93edc2270fec4a08d22d
azhimn <mazhn34@gmail.com>
2023-11-08 21:03:30 +0800

    Done project 'Go to URL or domain name'

--- src/script.js
+++ src/script.js
@@ -1,5 +1,23 @@
-function search() {
-  console.log('asd');
+function search(event) {
   const search = document.getElementById('search');
-  window.location.replace = "https://duckduckgo.com/?q=" + search.value;
+  const greet = document.getElementById('greet');
+  const style = getComputedStyle(document.body)
+  let searchValue = search.value;
+  let greetValue = greet.value;
+  
+  // console.log(greet.style['color'] = style.getPropertyValue('--color-font-muted'));
+
+  if (event.key == 'Enter') {
+    const domainRegex = /^([a-zA-Z0-9-]+\.){1,}[a-zA-Z]{2,}$/;
+    const urlRegex = /^(http|https|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+  
+    if (domainRegex.test(searchValue)) {
+      window.location.href = "https://" + searchValue;
+    } else if (urlRegex.test(searchValue)) {
+      window.location.href = searchValue;
+    } else {
+      window.location.href = "https://duckduckgo.com/?q=" + searchValue;
+    }
+  }
 }
+
